9d40d8d29caf24d9db6052754c539710bddbc6cd
Rocco <kylerocco08@gmail.com>
2024-01-25 09:53:02 -0500

    Strings

---
+++ Strings/strings.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+#Programmer: Kyle Rocco
+#Program: Strings
+#Date: 1.8.11
+
+print("Michigan Sucks, JJ Mcarthy is the worst player of all time and is carried")
+print('\n') #\n = skips a line
+print("7 + 8 =\n ", 7 + 8)
+print("10 + 8 = "+ str(10 + 8),"\n")
+print("""\nHappy New Year!
+everyone!!!""") #Triple quotes for multiple lines
