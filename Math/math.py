44fa3a6c3b00aa6de33b1200c21d0912aedc3d4a
Rocco <kylerocco08@gmail.com>
2024-01-25 09:48:30 -0500

    Math

---
+++ Math/math.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+#Programmer: Kyle Rocco
+#Program: Math
+#Date: 1.10.24
+
+print(50+50)
+print(50*50)
+print(50/3)
+print(50-50)
+print(50+50-50*50/50) #PEMDAS
+print(50**50) #exponent
+print(50%3) #modulo - division remainder
+print(50 / 3) #division with remainder
+print(50 // 3) #whole number no remainder
